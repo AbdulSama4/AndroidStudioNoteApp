f0ad2b8616778e57566aaf524a1b68f1bcccc4d8
brawndavis18 <149895066+brawndavis18@users.noreply.github.com>
2024-02-28 16:17:12 -0500

    Update ReminderDataSource.java

--- app/src/main/java/com/example/myapplication/ReminderDataSource.java
+++ app/src/main/java/com/example/myapplication/ReminderDataSource.java
@@ -1,37 +1,33 @@
-package com.example.myapplication;
+package com.example.mobileappdevproject;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class ReminderDataSource {
 
     private SQLiteDatabase database;
-
     private final ReminderDBHelper dbHelper;
 
     public ReminderDataSource(Context context) {
         dbHelper = new ReminderDBHelper(context);
     }
-
     public void open() throws SQLException {
         database = dbHelper.getWritableDatabase();
     }
-
     public void close() {
         dbHelper.close();
     }
-
     public boolean insertReminder(Reminder r) {
         boolean didSucceed = false;
         try {
             ContentValues initialValues = new ContentValues();
 
+            String subject = (r.getSubject() != null) ? r.getSubject() : "DefaultSubject";
             initialValues.put("subject", r.getSubject());
             initialValues.put("description", r.getDescription());
             initialValues.put("priority", r.getPriority());
@@ -39,14 +35,14 @@ public class ReminderDataSource {
 
 
             didSucceed = database.insert("reminder", null, initialValues) > 0;
-
         } catch (Exception e) {
+            e.printStackTrace();
             //Do nothing will return fasle if there is an exception
         }
         return didSucceed;
-
-
     }
+
+
 
     public boolean updateReminder(Reminder r) {
         boolean didSucceed = false;
@@ -60,9 +56,7 @@ public class ReminderDataSource {
             updateValues.put("date", String.valueOf(r.getDate().getTimeInMillis()));
 
             didSucceed = database.update("reminder", updateValues, "_id=" + rowId, null) > 0;
-
         } catch (Exception e) {
-
         }
         return didSucceed;
     }
@@ -72,7 +66,6 @@ public class ReminderDataSource {
         try {
             String query = "Select MAX(_id) from reminder";
             Cursor cursor = database.rawQuery(query, null);
-
             cursor.moveToFirst();
             lastId = cursor.getInt(0);
             cursor.close();
@@ -105,7 +98,6 @@ public class ReminderDataSource {
         try {
             String query = "SELECT * FROM reminder ORDER BY" + sortField + " " + sortOrder;
             Cursor cursor = database.rawQuery(query, null);
-
             Reminder newReminder;
             cursor.moveToFirst();
             while (!cursor.isAfterLast()) {
@@ -127,12 +119,10 @@ public class ReminderDataSource {
         }
         return reminders;
     }
-
     public Reminder getSpecificReminder(int reminderId) {
         Reminder reminder = new Reminder();
         String query = "SELECT * FROM reminder WHERE _id = " + reminderId;
         Cursor cursor = database.rawQuery(query, null);
-
         if (cursor.moveToFirst()) {
             reminder.setReminderID(cursor.getInt(0));
             reminder.setSubject(cursor.getString(1));
@@ -142,21 +132,20 @@ public class ReminderDataSource {
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(Long.valueOf(cursor.getString(4)));
             reminder.setDate(calendar);
-
             cursor.close();
-
         }
         return reminder;
     }
-
     public boolean deleteReminder(int reminderId) {
         boolean didDelete = false;
         try {
             didDelete = database.delete("reminder", "_id=" + reminderId, null) > 0;
         } catch (Exception e) {
-
         }
         return didDelete;
+    }
+}
+
     }
 }
 
