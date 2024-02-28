fb3db0a9d891555dbc03168042f8419199b34286
brawndavis18 <149895066+brawndavis18@users.noreply.github.com>
2024-02-28 16:16:32 -0500

    Update Reminder.java

--- app/src/main/java/com/example/myapplication/Reminder.java
+++ app/src/main/java/com/example/myapplication/Reminder.java
@@ -1,12 +1,10 @@
-package com.example.myapplication;
+package com.example.mobileappdevproject;
 
 import java.util.Calendar;
 
 public class Reminder {
     private int reminderID;
-
     private String subject;
-
     private String description;
 
     private Calendar date;
@@ -17,44 +15,37 @@ public class Reminder {
         reminderID = -1;
         date = Calendar.getInstance();
         priority = -1;
+        subject = "Default Subject";
+        description = null;
     }
-
     public int getReminderID() {
         return reminderID;
     }
-
     public void setReminderID(int reminderID) {
         this.reminderID = reminderID;
     }
-
     public String getSubject() {
+
         return subject;
     }
-
     public void setSubject(String subject) {
         this.subject = subject;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public int getPriority() {
         return priority;
     }
-
     public void setPriority(int priority) {
         this.priority = priority;
     }
-
     public Calendar getDate() {
         return date;
     }
-
     public void setDate(Calendar date) {
         this.date = date;
     }
